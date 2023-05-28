0899e31c27e9a60b2d234a305a9b21f212e52b95
Yifan Qian <603934493@qq.com>
2023-05-28 12:51:58 -0500

    forum: add, update and flash attribute

--- src/main/java/com/qyf/spring_study/web/BookController.java
+++ src/main/java/com/qyf/spring_study/web/BookController.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -29,5 +32,47 @@ public class BookController {
         }
         model.addAttribute("book", book);
         return "book";
+    }
+
+    /**
+     * Rediret to input pafe
+     * @return
+     */
+    @GetMapping("/books/input")
+    public String inputPage(Model model){
+        model.addAttribute("book", new Book());
+        return "input";
+    }
+
+    @GetMapping("/books/{id}/input")
+    public String inputEditPage(@PathVariable long id, Model model){
+        Book book = bookService.findOne(id).get();
+        model.addAttribute("book", book);
+        return "input";
+    }
+
+    /**
+     * submit a book information
+     * POST -->redirect -->GET
+     * MuST use flash attribute, can not use model.attribute only
+     */
+    @PostMapping("/books")
+    public String post(
+            @RequestParam long id,
+            @RequestParam String name,
+            @RequestParam String author,
+            @RequestParam String description,
+            @RequestParam int status,
+            final RedirectAttributes attributes
+            ){
+        Book book= new Book();
+        book.setName(name);
+        book.setStatus(status);
+        book.setDescription(description);
+        book.setAuthor(author);
+        book.setId(id);
+        bookService.save(book);
+        attributes.addFlashAttribute("message", "<<" + book.getName() + ">> information submitted successfully");
+        return "redirect:/books";
     }
 }
